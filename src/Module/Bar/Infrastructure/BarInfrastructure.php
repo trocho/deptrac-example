0289321662d8113f3a1422124882b55f7a49091c
trocho <patryk.trocho@gmail.com>
2022-08-18 20:17:22 +0200

    aa sample for MVC architecture

--- src/Module/Bar/Infrastructure/BarInfrastructure.php
+++ src/Module/Bar/Infrastructure/BarInfrastructure.php
@@ -4,7 +4,12 @@ declare(strict_types=1);
 
 namespace Module\Bar\Infrastructure;
 
+use Module\Mvc\MvcFacade;
+
 class BarInfrastructure
 {
-
+    public function someIntegration(): void
+    {
+        $mvcFacade = new MvcFacade();
+    }
 }
